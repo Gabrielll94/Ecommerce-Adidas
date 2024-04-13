61a97a2994871b74a37c302f45c4274a7725ac28
unknown <jgabriel.olivares1994@gmail.com>
2024-04-13 11:31:58 -0300

    conexion a la BDD

--- APi/routes/productsRoutes.js
+++ APi/routes/productsRoutes.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../index");
 
 router.get("/", (req, res) => {
-  res.send("Obtener todos los productos");
+  pool.query("SELECT * FROM productos", (err, results) => {
+    if (err) {
+      console.error("Error al ejecutar la consulta SQL", err);
+      return res.status(500).send("Error interno del servidor");
+    }
+    res.json(results.rows);
+  });
 });
 
 router.get("/:id", (req, res) => {
   const productId = req.params.id;
-  res.send("Producto con id " + productId);
+  pool.query(
+    `SELECT * FROM productos WHERE id=$1`,
+    [productId],
+    (err, result) => {
+      if (err) {
+        console.error("Error al ejecutar la consulta SQL", err);
+        return res.status(500).send("Error interno del servidor");
+      }
+      if (results.rows === 0) {
+        res.status(404).send("No se ha encontrado el producto con ese ID");
+      }
+      res.json(results.rows[0]);
+    }
+  );
 });
 
 module.exports = router;
